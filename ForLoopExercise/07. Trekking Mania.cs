bc19f1defbc67ac572b4c4d7a5a7a6f90ef7afd4
Donika <121803093+DonikaMarcheva@users.noreply.github.com>
2023-01-08 06:56:27 +0200

    Add files via upload

---
+++ ForLoopExercise/07. Trekking Mania.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _07._Trekking_Mania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p1=0;
+            int p2=0;
+            int p3=0;
+            int p4=0;
+            int p5=0;
+            int people;
+
+            for (int i = 1; i <=n; i++)
+            {
+                people = int.Parse(Console.ReadLine());
+                if (people <=5 )
+                {
+                    p1 += people;
+                }
+                else if (people <=12 )
+                {
+                    p2 += people;
+                }
+                else if (people <= 25)
+                {
+                    p3 += people;
+                }
+                else if (people <= 40)
+                {
+                    p4 += people;
+                }
+                else
+                {
+                    p5+=people;
+                }
+            }
+            int sumPeople = p1 + p2 + p3 + p4 + p5;
+            
+            Console.WriteLine($"{(double)p1 / sumPeople * 100:f2}%");
+            Console.WriteLine($"{(double)p2 / sumPeople * 100:f2}%");
+            Console.WriteLine($"{(double)p3 / sumPeople * 100:f2}%");
+            Console.WriteLine($"{(double)p4 / sumPeople * 100:f2}%");
+            Console.WriteLine($"{(double)p5 / sumPeople * 100:f2}%");
+
+
+        }
+    }
+}
---
+++ ForLoopExercise/08. Tennis Ranklist.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _08._Tennis_Ranklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            int points=int.Parse(Console.ReadLine());
+            string position;
+            int winnerPosition = 0;
+            int winnedPoints=0;
+
+            for (int i = 1; i <=n; i++)
+            {
+                position = Console.ReadLine();
+
+                if (position=="W")
+                {
+                    points += 2000;
+                    winnerPosition++;
+                    winnedPoints += 2000;
+                }
+                else if (position=="F")
+                {
+                    points += 1200;
+                    winnedPoints += 1200;
+                }
+                else
+                {
+                    points += 720;
+                    winnedPoints+=720;
+                }
+            }
+            Console.WriteLine($"Final points: {points}");
+            Console.WriteLine($"Average points: {Math.Floor((double)winnedPoints/ n)}");
+
+            Console.WriteLine($"{(double)winnerPosition / n * 100:f2}%");
+        }
+    }
+}
