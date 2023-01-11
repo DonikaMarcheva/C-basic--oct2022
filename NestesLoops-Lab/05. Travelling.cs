e4d00e21562fbf5551ecffaf4436580826fa5d5f
Donika <121803093+DonikaMarcheva@users.noreply.github.com>
2023-01-11 08:56:48 +0200

    Add files via upload

---
+++ NestesLoops-Lab/05. Travelling.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double money;
+            string destination;
+            
+            while ((destination = Console.ReadLine()) != "End")
+            {
+                double budget = double.Parse(Console.ReadLine());
+                
+              while ((money = double.Parse(Console.ReadLine()))< budget)
+                {
+                    budget-=money;
+                    
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+            }
+            
+
+        }
+    }
+}
